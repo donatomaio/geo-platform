44caec7fd610222c11d3b811fc65ab6e050e77e3
donato.maio <donato.maio@gmail.com>
2021-02-19 10:48:44 +0100

    set forcerule to false

--- geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/legend/GPLegendWidget.java
+++ geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/legend/GPLegendWidget.java
@@ -102,7 +102,7 @@ public class GPLegendWidget {
             if (!authkeyTuple.equals("")) {
                 imageURL.append('&').append(authkeyTuple);
             }
-            imageURL.append("&LEGEND_OPTIONS=forceRule:True;forceLabels:on");
+            imageURL.append("&LEGEND_OPTIONS=forceRule:False;forceLabels:on");
             image = new Image(imageURL.toString());
             cp.add(image);
             this.legendsStore.add(cp);
